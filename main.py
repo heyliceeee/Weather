36693095315b861176aeec440471c3dce049ff9e
Alice Dias <58340504+heyliceeee@users.noreply.github.com>
2026-05-13 15:07:12 +0100

    ✨feat: get monday temperature and convert to fahrenheit

--- main.py
+++ main.py
@@ -14,13 +14,18 @@ def get_data_from_csv():
     return pandas.read_csv(file_path)
 
 data = get_data_from_csv()
-print(data)
-print(float(data["temp"].mean())) # average temperature
+print("\ndata: \n", data)
 
-# max value temperature
-max_temperature = float(data["temp"].max())
-print(max_temperature)
+print("\naverage temperature: ", float(data["temp"].mean())) # average temperature
 
-# data who most max temperature
-temperatures = data["temp"]
-print(data[temperatures == max_temperature])
+max_temperature = float(data["temp"].max()) # max value temperature
+print("\nmax temperature: ", max_temperature)
+
+temperatures = data["temp"] # temperature column
+day_highest_temperature = data[data.temp == max_temperature] # data who day with the highest temperature
+print("\nday with the highest temperature: \n", day_highest_temperature)
+
+monday = data[data.day == "Monday"] # data who day is Monday
+monday_temperature = float(monday.temp[0]) # monday temperature
+fahrenheit = (monday_temperature * 1.8) + 32 # convert temperature to Fahrenheit
+print("monday temperature in fahrenheit: ", fahrenheit)
